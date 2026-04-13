72e2698aab4583a3472adf225b52f8d59abf414b
JuanFer <juan.narvaez@chatbet.gg>
2026-04-13 09:33:53 -0500

    fix: change api-key on site_config

--- chatbet_base_models/site_config_model.py
+++ chatbet_base_models/site_config_model.py
@@ -422,6 +422,9 @@ class SiteConfig(BaseModel):
             ),
         )
     )
+    api_key: Optional[str] = Field(
+        default=None, description="API key for public endpoints"
+    )
 
     @classmethod
     def default_factory(cls, site_name: str, company_id: str) -> SiteConfig:
@@ -436,7 +439,6 @@ class SiteConfigDB(SiteConfig):
     SK: Optional[str] = Field(default=None, description="Sort key")
     created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
     updated_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
-    api_key: Optional[str] = Field(default=None, description="API key for public endpoints")
 
     @classmethod
     def default_factory(cls, site_name: str, company_id: str) -> SiteConfigDB:
